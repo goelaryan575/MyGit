3904d8d7b546af6ef5627d0143e23967a8ced664
Aryan Goel <aryangoel@Aryans-MacBook-Air.local>
2022-10-06 18:35:09 +0530

    new

---
+++ link list/link list/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  link list
+//
+//  Created by Aryan Goel on 05/10/22.
+//
+
+#include <stdio.h>
+
+int main() {
+    for (int i=0; i<23; i++) {
+        printf("%d",i);
+    }
+    int n=88;
+    printf("%d",n);
+    return 0;
+}
